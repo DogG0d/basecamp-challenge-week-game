7c6294c20485d77343e26cf052226940a4cb9826
Jaron Lugtenburg <jaronlugtenburg@dogg0d.dev>
2024-10-10 16:53:31 +0200

    Tilemap system, player movement & camera adjustment, everything else (tweaks, cleanups)

--- game.py
+++ game.py
@@ -2,6 +2,7 @@
 
 # Locals
 import sys
+import os
 # import time
 # Game engine
 import pygame
@@ -10,8 +11,10 @@ import constant
 from constant import *
 from inputstream import InputStream
 import sprites
+# Game scripts
 from scripts.utils import load_image, load_images
 from scripts.tilemap import Tilemap
+from scripts.entities import PhysicsEntity
 from scripts.clouds import Clouds
 
 class Game():
@@ -23,9 +26,14 @@ class Game():
         # Game clock
         self.clock = pygame.time.Clock()
 
-        # Display
-        self.display = pygame.display.set_mode(SCREEN_SIZE)
+        # Game window
+        self.display = pygame.display.set_mode(DISPLAY_SIZE)
+        self.is_full_screen = False
         pygame.display.set_caption("Challangeweek - Name Unknown")
+
+        # Game surface
+        self.screen = pygame.Surface(constant.GAME_SIZE)
+
         # Assets
         self.assets = {  # Be careful with photos that contain a lot of black: Black is CHROMA KEYED OUT for transparency
             "player": load_image("entities/player.png"),
@@ -35,51 +43,124 @@ class Game():
             "grass": load_images("tiles/grass"),
             "clouds": load_images("clouds")
         }
+
+        # Tilemap
+        self.tilemap = Tilemap(self, tile_size=16)
+        
         # Entities
         self.player = PhysicsEntity(self, "player", (50, 50), (8, 15))
 
         # Clouds
         self.clouds = Clouds(self.assets["clouds"], count=16)
+
+        # Input stream
+        self.input = InputStream()
+
+        # Camera offset
+        self.scroll = [0, 0]
     
 
     def run(self):
         while True:
+            ### START Game rendering
+
+            # Camera scroll
+            self.scroll[0] += (self.player.copy_rect().centerx - self.screen.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.copy_rect().centery - self.screen.get_height() / 2 - self.scroll[1]) / 30
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            # Reset screen
+            self.screen.fill(RGB.SKY_BLUE)
+
             # Clouds
             self.clouds.update()
             self.clouds.render(self.screen, render_scroll)
 
+            # Tilemap
+            self.tilemap.render(surf=self.screen, offset=render_scroll)
+
             # Entities
             self.player.render(surf=self.screen, offset=render_scroll)
 
-            pygame.display.update()
-            self.clock.tick(constant.FRAMES_PER_SECOND)
+            # HUD
 
-            # Update cooldowns
-            dash_cooldown = min(dash_cooldown + 1, constant.DASH_COOLDOWN_FRAMES)
-            if player_one.vel_x == 0:
-                is_dashing = False
+            ### END Game rendering
+
+            ### Event grabbing
+            events = pygame.event.get()
             
-            ## DEBUG
-            # print()
+            ### Update keypresses
+            self.input.processInput()
+
+            ### Event handling
+
+            # Quit
+            if self.input.isKeyPressed(pygame.K_q) or list(filter(lambda event: event.type == pygame.QUIT, events)):
+                pygame.quit()
+                sys.exit()
+            
+            ### Keypress handling
+
+            # Resize
+            if self.input.isKeyPressed(pygame.K_F11):
+                if self.is_full_screen:
+                    pygame.display.set_mode(pygame.display.get_window_size(), 0)
+                    pygame.display.set_mode(constant.DISPLAY_SIZE, 0)
+                    self.is_full_screen = False
+                else:
+                    pygame.display.set_mode(pygame.display.get_desktop_sizes()[0], pygame.FULLSCREEN)
+                    self.is_full_screen = True
+            
+            ## Player move
+            move_x = move_y = 0
+
+            # Walk right
+            if self.input.isKeyDown(pygame.K_d) or self.input.isKeyDown(pygame.K_RIGHT):
+                move_x += 2
+            
+            # Walk left
+            if self.input.isKeyDown(pygame.K_a) or self.input.isKeyDown(pygame.K_LEFT):
+                move_x -= 2
+            
+            # Jump
+            if self.input.isKeyPressed(pygame.K_SPACE) or self.input.isKeyPressed(pygame.K_UP) or self.input.isKeyPressed(pygame.K_w):
+                self.player.vel[1] = -constant.JUMP_HEIGHT
+
+            ### Location updating
+
+            self.player.update(self.tilemap, (move_x, move_y))
+
+
+            # FINAL Updating display
+            self.display.blit(pygame.transform.scale(self.screen, self.display.get_size()), (0, 0))
+
+            pygame.display.update()
+
+            ### Game clock update
+            self.clock.tick(constant.FRAMES_PER_SECOND)
+    
+
+    def get_assets(self) -> dict[str, pygame.surface.Surface]:
+        return self.assets
 
 
 # frame = display.get_rect()
 # camera = frame.copy()
-font_36 = pygame.font.SysFont(pygame.font.get_default_font(), 36)
+# font_36 = pygame.font.SysFont(pygame.font.get_default_font(), 36)
 
-# Input manager
-input = InputStream()
+# # Input manager
+# input = InputStream()
 
-# Floor
-floor_group = pygame.sprite.Group()
-floor_group.add(sprites.Base(0, 800, 1600, 100, RGB.GRAY))
-floor_group.add(sprites.Base(1700, 800, 1600, 100, RGB.GRAY))
-floor_group.add(sprites.Base(1200, 700, 1600, 100, RGB.GRAY))
+# # Floor
+# floor_group = pygame.sprite.Group()
+# floor_group.add(sprites.Base(0, 800, 1600, 100, RGB.GRAY))
+# floor_group.add(sprites.Base(1700, 800, 1600, 100, RGB.GRAY))
+# floor_group.add(sprites.Base(1200, 700, 1600, 100, RGB.GRAY))
 
-# Playersdddddddd
-player_group = pygame.sprite.Group()
-player_one = sprites.Player(400, 100, 50, 50, RGB.ORANGE, constant.PLAYER_HEALTH)
-player_group.add(player_one)
+# # Playersdddddddd
+# player_group = pygame.sprite.Group()
+# player_one = sprites.Player(400, 100, 50, 50, RGB.ORANGE, constant.PLAYER_HEALTH)
+# player_group.add(player_one)
 
 # player = pygame.Rect(375, 100, 50, 50)
 # player_vel_x = 0
@@ -89,10 +170,9 @@ player_group.add(player_one)
 # player_collision_x = False
 # player_collision_y = False
 
-dash_cooldown = constant.DASH_COOLDOWN_FRAMES
+# dash_cooldown = constant.DASH_COOLDOWN_FRAMES
 
-is_on_ground = False
-can_double_jump = False
-is_dashing = False
-movement_direction = "right"
-
+# is_on_ground = False
+# can_double_jump = False
+# is_dashing = False
+# movement_direction = "right"
---
+++ scripts/tilemap.py
@@ -0,0 +1,48 @@
+import pygame
+import game
+
+NEIGHBOUR_OFFSETS = [(-1, -1), (0, -1), (1, -1), (-1, 0), (0, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+PHYSICS_TILES = {"grass", "stone"}
+
+class Tilemap():
+    def __init__(self, game: "game.Game", tile_size: int = 16):
+        self.game = game
+        self.tile_size = tile_size
+        self.tilemap = {}
+        self.offgrid_tiles = []
+        self.game
+
+        for i in range(10):
+            self.tilemap[f"{3 + i};10"] = {"type": "grass", "variant": 1, "pos": (3 + i, 10)}
+            self.tilemap[f"10;{5 + i}"] = {"type": "stone", "variant": 1, "pos": (10, 5 + i)}
+    
+    def get_tiles_around(self, pos: tuple[int, int]) -> list[dict]:
+        tiles = []
+        tile_loc = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size))
+        for offset in NEIGHBOUR_OFFSETS:
+            if (check_loc := f"{tile_loc[0] + offset[0]};{tile_loc[1] + offset[1]}") in self.tilemap:
+                tiles.append(self.tilemap[check_loc])
+        
+        return tiles
+
+
+    def physics_rect_around(self, pos: tuple[int, int]) -> list[pygame.Rect]:
+        rects = []
+        for tile in self.get_tiles_around(pos):
+            if tile["type"] in PHYSICS_TILES:
+                rects.append(pygame.Rect(tile["pos"][0] * self.tile_size, tile["pos"][1] * self.tile_size, self.tile_size, self.tile_size))
+        return rects
+
+
+    def render(self, surf: pygame.Surface, offset: tuple[int, int] = (0,0)):
+        for tile in self.offgrid_tiles:
+            surf.blit(self.game.get_assets()[tile["type"]][tile["variant"]], (tile["pos"][0] - offset[0], tile["pos"][1] - offset[1]))
+
+        for tile_x in range(offset[0] // self.tile_size, (offset[0] + surf.get_width()) // self.tile_size + 1):
+            for tile_y in range(offset[1] // self.tile_size, (offset[1] + surf.get_height()) // self.tile_size + 1):
+                loc = f"{tile_x};{tile_y}"
+                if loc in self.tilemap:
+                    tile = self.tilemap[loc]
+                    surf.blit(self.game.get_assets()[tile["type"]][tile["variant"]], (tile["pos"][0] * self.tile_size - offset[0], tile["pos"][1] * self.tile_size - offset[1]))
+            
+
