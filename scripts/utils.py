04884a0ee5620d3a2b1fca6afeedd1bc5f9f29b8
Jaron Lugtenburg <jaronlugtenburg@dogg0d.dev>
2024-11-07 18:00:40 +0100

    Add particles to assets loader @Felix

--- scripts/utils.py
+++ scripts/utils.py
@@ -38,6 +38,8 @@ def load_assets(path: str) -> dict[str, pygame.Surface | list[pygame.Surface] | 
                     assets.update({asset: load_images(properties["path"])})
                 case "file":
                     assets.update({asset: load_image(properties["path"])})
+                case "particle":
+                    assets.update({asset: Animation(load_images(properties["path"]), properties["frame_duration"], properties["looping"])})
                 case _:
                     raise Exception(f"Asset named \"{properties["type"]}\" is invalid.")
     
